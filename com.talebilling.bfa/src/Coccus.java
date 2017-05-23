01f1432707effaadc2b51d9c0b47856fd8461805
Emese Billing <talebilling@gmail.com>
2017-05-23 12:50:24 +0200

    minor change

--- com.talebilling.bfa/src/Coccus.java
+++ com.talebilling.bfa/src/Coccus.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Created by eb on 2017.05.23..
  */
@@ -5,6 +7,10 @@ public class Coccus extends Bacterium {
 
     public Coccus () {
         super(10, 0.1f);
+    }
 
+    @Override
+    public boolean checkIfDead(List<Bacterium> petri) {
+        return false;
     }
 }
--- com.talebilling.bfa/src/Spirillum.java
+++ com.talebilling.bfa/src/Spirillum.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Created by eb on 2017.05.23..
  */
@@ -6,5 +8,10 @@ public class Spirillum extends Bacterium {
     public Spirillum () {
         super(4, 0.2f);
 
+    }
+
+    @Override
+    public boolean checkIfDead(List<Bacterium> petri) {
+        return false;
     }
 }
